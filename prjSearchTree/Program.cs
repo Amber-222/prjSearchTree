a9054c9b81dd196c18932f7f339cc2a9e36f0ef1
Amber-222 <ST10287325@vcconnect.edu.za>
2025-08-26 19:03:32 +0200

    bfs and dfs search algorithms

---
+++ prjSearchTree/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace prjSearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<string> orgChart = new Tree<string>();
+            orgChart.Root = new TreeNode<string> { Data = "CEO" };
+
+            var vp = new TreeNode<string> { Data = "VP", Parent = orgChart.Root };
+            var manager = new TreeNode<string> { Data = "manager", Parent = orgChart.Root };
+
+            orgChart.Root.Children.AddRange(new[] { vp, manager });
+
+            var jeff = new TreeNode<string> { Data = "Jeff", Parent = vp };
+            vp.Children.Add(jeff);
+
+            var john = new TreeNode<string> { Data = "John", Parent = vp };
+            vp.Children.Add(john);
+
+            var kim = new TreeNode<string> { Data = "Kim", Parent = manager };
+            manager.Children.Add(kim);
+
+            Console.WriteLine("---- Org Chart Structure ----");
+            orgChart.PrintTree(orgChart.Root);
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("------------ BFS ------------");
+            List<string> bfsResult = orgChart.BreadthFirstSearch();
+            Console.WriteLine(string.Join(" -> ", bfsResult));
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("------------ BFS ------------");
+            List<string> dfsResult = orgChart.DepthFirstSearch();
+            Console.WriteLine(string.Join(" -> ", dfsResult));
+            Console.WriteLine("-----------------------------");
+
+            #region
+            //IN CLASS EXAMPLE, IN WORD DOC
+            Tree<string> complexTree = new Tree<string>();
+            complexTree.Root = new TreeNode<string> { Data = "A" }; //ROOT
+
+            //A'S CHILDREN
+            var J = new TreeNode<string> { Data = "J", Parent = complexTree.Root };
+            var B = new TreeNode<string> { Data = "B", Parent = complexTree.Root };
+            var Y = new TreeNode<string> { Data = "Y", Parent = complexTree.Root };
+            var H = new TreeNode<string> { Data = "H", Parent = complexTree.Root };
+            var G = new TreeNode<string> { Data = "G", Parent = complexTree.Root };
+            complexTree.Root.Children.AddRange(new[]  { J, B, Y, H, G });
+
+            //J'S CHILDREN
+            var Z = new TreeNode<string> { Data = "Z", Parent = J };
+            var C = new TreeNode<string> { Data = "C", Parent = J };
+            var K = new TreeNode<string> { Data = "K", Parent = J };
+            J.Children.AddRange(new[] {Z, C, K });
+
+            //C'S CHILDREN
+            var X = new TreeNode<string> { Data = "X", Parent = C };
+            var N = new TreeNode<string> { Data = "N", Parent = C };
+            C.Children.AddRange(new[] { X, N });
+
+            //K'S CHILD
+            var L = new TreeNode<string> { Data = "L", Parent = K };
+            K.Children.Add(L);
+
+            //B'S CHILDREN
+            var D = new TreeNode<string> { Data = "D", Parent = B };
+            var F = new TreeNode<string> { Data = "F", Parent = B };
+            B.Children.AddRange(new[] { D, F });
+
+            //G'S CHILD
+            var M = new TreeNode<string> { Data = "M", Parent = G };
+            G.Children.Add(M);
+
+            //D'S CHILD
+            var E = new TreeNode<string> { Data = "E", Parent = D };
+            D.Children.Add(E);
+
+            //F'S CHILD
+            var I = new TreeNode<string> { Data = "I", Parent = F };
+            F.Children.Add(I);
+            #endregion
+
+            Console.WriteLine("---- Complex Tree Structure ----");
+            orgChart.PrintTree(complexTree.Root);
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("------------ BFS ------------");
+            List<string> bfsResultComplex = complexTree.BreadthFirstSearch();
+            Console.WriteLine(string.Join(" -> ", bfsResultComplex));
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("------------ BFS ------------");
+            List<string> dfsResultComplex = complexTree.DepthFirstSearch();
+            Console.WriteLine(string.Join(" -> ", dfsResultComplex));
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
---
+++ prjSearchTree/Tree.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjSearchTree
+{
+    public class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+
+        public void PrintTree(TreeNode<T> node, string indent = "", bool last = true)
+        {
+            if (node == null) return;
+            Console.WriteLine($"{indent}+-{node.Data}");
+            indent += last ? "  " : "| ";
+
+            for (int i = 0; i < node.Children.Count; i++)
+            {
+                PrintTree(node.Children[i], indent, i == node.Children.Count - 1);
+            }
+        }
+
+        public TreeNode<T> FindNode(TreeNode<T> node, T value)
+        {
+            if (node == null) return null; //if the node passed in is empty it returns empty
+
+            if (node.Data.Equals(value)) return node; //else if the current given node already matches the value looking form return it as is
+
+            foreach (var child in node.Children) //loop through all the children of the given node until the value is found
+            {
+                var result = FindNode(child, value); //run the method to look at the value of each of the children 
+                if (result != null) return result; //until it finally finds a value that matches the parameters
+            }
+            return null;
+        }
+
+        public void PrintTree(TreeNode<T> node, TreeNode<T> highlightNode, string indent = "", bool last = true)
+        {
+            if (node == null) return;
+
+            if (node.Equals(highlightNode))
+            {
+                Console.WriteLine($"{indent}+- [{node.Data}]");
+            }
+            else
+            {
+                Console.WriteLine($"{indent}+- {node.Data}");
+            }
+
+            indent += last ? "   " : "| ";
+
+            for (int i = 0; i < node.Children.Count; i++)
+            {
+                PrintTree(node.Children[i], highlightNode, indent, i == node.Children.Count - 1);
+            }
+        }
+
+        //BREADTH FIRST SEARCH
+        public List<T> BreadthFirstSearch()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            var visited = new List<T>(); //stores which nodes have been looked at 
+            var queue = new Queue<TreeNode<T>>(); //use a queue for bfs
+            queue.Enqueue(Root);
+
+            while (queue.Count > 0)
+            {
+                TreeNode<T> currentNode = queue.Dequeue(); //get next node in the queue
+                visited.Add(currentNode.Data); //visit it, store that its been looked at
+
+                //add all its children at the back of the queue
+                foreach (var child in currentNode.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+            return visited;
+        }
+
+        //DEPTH FIRST SEARCH
+        public List<T> DepthFirstSearch()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            var visited = new List<T>();
+            dfsHelper(Root, visited); //start recursive search from root node
+            return visited;
+        }
+
+        //dfs helper method
+        private void dfsHelper(TreeNode<T> node, List<T> visited)
+        {
+            if (node == null) return;
+
+            visited.Add(node.Data); //viist current node
+
+            //recursively visit each child to delve deeper into the tree
+            foreach (var child in node.Children)
+            {
+                dfsHelper(child, visited);
+            }
+        }
+    }
+}
